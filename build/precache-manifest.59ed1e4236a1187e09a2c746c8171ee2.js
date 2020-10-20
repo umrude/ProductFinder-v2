3a3671863fe8557481be0cefd272ccbd442a2fed
Gabi Geollegue <gabigeollegue@MacBook-Pro-2.local>
2020-10-20 14:04:30 -0700

    add bulild file

---
+++ build/precache-manifest.59ed1e4236a1187e09a2c746c8171ee2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a318425ae9fadf4e7b4354637aea4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "398e0d718cffc6aa64dc",
+    "url": "/static/css/main.ba77ffca.chunk.css"
+  },
+  {
+    "revision": "d78977853f404a5ad060",
+    "url": "/static/js/2.eaea5078.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.eaea5078.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "398e0d718cffc6aa64dc",
+    "url": "/static/js/main.21c9c6c6.chunk.js"
+  },
+  {
+    "revision": "79c4a16b25e600862ac9",
+    "url": "/static/js/runtime-main.6d9141b5.js"
+  }
+]);
